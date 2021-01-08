c977d38d4901748547d5042d6dc5757a1331422f
Russell <russelldev@protonmail.com>
2021-01-08 10:01:38 +0000

    Add delay to incrementation

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ counters.forEach(counter => {
     counter.innerText = '0';
     const updateCounter = () => {
         const target = +counter.getAttribute('data-target');
-        const c = +counter.innerText;
-        
-        const increment = target / 200; 
-
+        const c = +counter.innerText;       
+        const increment = target / 500; 
         if (c < target) {
             counter.innerText = `${Math.ceil(c + increment)}`;
+            setTimeout(() => {
+                updateCounter()
+            }, 1);
         }
     }
     updateCounter();
-
 });
